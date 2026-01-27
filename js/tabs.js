fafa7e5a25dc890bd438d355b886a12a61255bc3
ALEKSANDR.I <107239203+AlexRMB1455AL@users.noreply.github.com>
2026-01-28 03:23:24 +0400

    endint

--- js/tabs.js
+++ js/tabs.js
@@ -14,9 +14,22 @@
 // })
 
 const tabButtons = document.querySelectorAll(".design-list__item");
-const tabDescription = document.querySelectorAll('.design-descr');
+const tabDescription = document.querySelectorAll(".design-descr");
+const tabImages = document.querySelectorAll(".design-images");
+
+const changeContent = (array, value) => {
+  array.forEach((elem) => {
+      if (elem.dataset.tabsField === value) {
+        elem.classList.remove("hidden");
+      } else {
+        elem.classList.add("hidden");
+      }
+    });
+};
+
 // console.log(tabButtons);
-console.log(tabDescription);
+// console.log(tabDescription);
+// console.log(tabImages);
 
 tabButtons.forEach((tabButton, index) => {
   //   tabButton.onclick = function () {
@@ -24,29 +37,41 @@ tabButtons.forEach((tabButton, index) => {
   //     console.dir(tabButton);
   //   };
   tabButton.addEventListener("click", (event) => {
+    // tabButton.dataset.active = '123';
+    const dataValue = tabButton.dataset.tabsHandler;
     tabDescription.forEach((descr, indexDescr) => {
-       if(index === indexDescr) {
-        
-            descr.classList.remove('hidden')
-        } else {
-            descr.classList.add('hidden')
-        }
-       
-    })
+      if (descr.dataset.tabsField === dataValue) {
+        descr.classList.remove("hidden");
+      } else {
+        descr.classList.add("hidden");
+      }
+    });
+
+    // tabImages.forEach((image) => {
+    //   if (image.dataset.tabsField === dataValue) {
+    //     image.classList.remove("hidden");
+    //   } else {
+    //     image.classList.add("hidden");
+    //   }
+    // });
     // console.log(event.target);
     // console.dir(tabButton);
     // event.target.classList.add('active');
-    tabButtons.forEach((btn) => {
-        // console.dir(tabButton);
-        // console.log(btn);
-        // console.log(event.target);
-        if(btn === event.target){
-            btn.classList.add('design-list_item.active')
-        } else {
-            btn.classList.remove('design-list_item.active')
+    changeContent(tabButtons, dataValue);
+    changeContent(tabDescription, dataValue);
+    changeContent(tabImages, dataValue);
 
-        }
-    })
+
+    tabButtons.forEach((btn) => {
+      // console.dir(tabButton);
+      // console.log(btn);
+      // console.log(event.target);
+      if (btn === event.currentTarge) {
+        btn.classList.add("design-list_item.active");
+      } else {
+        btn.classList.remove("design-list_item.active");
+      }
+    });
   });
   // tabButton.addEventListener('click', () => {
   //     console.log('click2');
