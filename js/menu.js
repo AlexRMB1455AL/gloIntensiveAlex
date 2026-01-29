bec6e02a4b1af6d5d64e04397abb1d4dd06a00c6
ALEKSANDR.I <107239203+AlexRMB1455AL@users.noreply.github.com>
2026-01-29 05:03:28 +0400

    create_accordeonjs

---
+++ js/menu.js
@@ -0,0 +1,26 @@
+const burger = document.querySelector('.humburger-menu');
+const menu = document.querySelector('.menu');
+const menuLinks = document.querySelectorAll('.menu-list__item');
+
+
+burger.addEventListener('click', () => {
+    // console.log('click');
+    menu.classList.add('menu-active');
+})
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('menu-active');
+    })  
+})
+
+document.addEventListener('click', (event) => {
+
+    if (!(event.target.closest('.menu') || event.target.closest('.humburger-menu'))) {
+        console.log('mobile menu');
+        menu.classList.remove('menu-active');
+    } 
+    // else {
+    //     console.log('past');
+    // }
+});
